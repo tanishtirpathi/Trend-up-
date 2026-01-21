1470de7a49e4d8fb005f3f43a5abf7036afd5179
tanish tirpathi <tanishtirpathi0@gmail.com>
2026-01-21 13:25:10 +0530

    sockets error

--- Backend/src/config/socket.js
+++ Backend/src/config/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 // for cors
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://trend-up.vercel.app","http://localhost:5173"],
   },
 });
 
