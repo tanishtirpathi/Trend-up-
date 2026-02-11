04903e97acf72a27ffd41f67bf3aedb28bafae20
tanish tirpathi <tanishtirpathi0@gmail.com>
2026-02-11 08:37:28 +0000

    code modify

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,18 +8,15 @@ import cors from "cors";
 import {app ,server} from "./config/socket.js"
 import rateLimit from "express-rate-limit";
 import morgan from "morgan";
-app.use(morgan("dev"));
+import compression from "compression";
 import helmet from "helmet";
 
-app.use(helmet());
-import compression from "compression";
-
-app.use(compression());
 
 //imports till here
 dotenv.config({
   path: "../env",
 });
+
 //cors issue
 app.use(
   cors({
@@ -27,9 +24,15 @@ app.use(
     credentials: true,
   })
 );
+
+// other things 
+app.use(helmet());//for sec
+app.use(compression());// for fast json data like compression type 
+app.use(morgan("dev"));// for good visibility 
+
+
 // rate limits 
 app.set("trust proxy", 1);
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 min
   max: 100, // max 100 requests per IP
@@ -40,14 +43,17 @@ const limiter = rateLimit({
   standardHeaders: true,
   legacyHeaders: false,
 });
-
 app.use(limiter);
+
 
 // just for json data
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 const PORT = process.env.PORT || 3000;
+
 //useless work
 connectDB(); //database connection;
 app.get("/health", (req, res) => {
@@ -59,6 +65,17 @@ app.use("/api/messages", MessageRoute); //Message routes
 app.get("/", (req, res) => {
   res.send("Backend is alive 🚀");
 });//healthCheck 
+
+//error handling global 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
+
 //real time 
 server.listen(PORT, () => {
   console.log(`server is running here : http://localhost:${PORT}`);
