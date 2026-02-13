eab4142574414a9411bfe6a845fdebbd50e2529f
tanish tirpathi <tanishtirpathi0@gmail.com>
2026-02-13 10:33:27 +0000

    url change

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -2,8 +2,8 @@ import { axiosInstants } from "../lib/axios";
 import { create } from "zustand";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:4000";
-//const BASE_URL = "https://trend-up-ipbl.onrender.com";
+//const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://trend-up-ipbl.onrender.com";
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
